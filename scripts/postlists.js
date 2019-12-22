4e50648ffe25c3c85e299bc08d0d48dc0ea4e1d0
Harshaganesha <harshaganesha5@gmail.com>
2019-12-22 17:59:19 +0530

    Added redirect functionality to ... button and added Post page

--- scripts/postlists.js
+++ scripts/postlists.js
@@ -1,4 +1,24 @@
+
+
+    document.getElementById("expand").onclick = function () {
+        window.location.href = '\post.html';
+    };
+    document.getElementById("expand1").onclick = function () {
+        window.location.href = '\post.html';
+    };
+    document.getElementById("expand2").onclick = function () {
+        window.location.href = '\post.html';
+    };
+    document.getElementById("expand3").onclick = function () {
+        window.location.href = '\post.html';
+    };
+    document.getElementById("expand4").onclick = function () {
+        window.location.href = '\post.html';
+    };
+
+
 function deletePost() {
+    event.stopPropagation();
     var par = document.querySelector(".fa-trash").closest(".col-6");
     par.remove();
 };
